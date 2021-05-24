cc765ec382d1ed6c655a492c440c3dec9d5898dd
Akif Hussain <62845231+Akif-Hussain@users.noreply.github.com>
2021-05-24 10:14:40 +0100

    Delete portfolio3-starter-template (1) directory

--- portfolio3-starter-template (1)/js/script.js
+++
@@ -1,8 +0,0 @@
-const nav=document.querySelector(".nav"),
-      navList=nav.querySelectorAll("li"),
-      totalNavList=navList.length;
-
-for(leti=0; i<totalNavList; i++){
-    const a=navList[i].querySelector("a");
-   console.log(a)
-}
